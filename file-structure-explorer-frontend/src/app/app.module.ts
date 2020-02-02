b11719e87c81642d5e2cea507ee6bdd6c08cd30e
Dmytro Hrynevych <grinevich97@gmail.com>
2020-02-02 17:53:35 +0200

    Create basic markup

--- file-structure-explorer-frontend/src/app/app.module.ts
+++ file-structure-explorer-frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule, MatTreeModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
